63963ede3649ee24c0adc49d07ac662508bbfaaa
iremlaletu <iremlaletu@gmail.com>
2024-12-14 19:58:58 +0300

    testeventonmobile

--- src/components/UI/TrainerCalendar.jsx
+++ src/components/UI/TrainerCalendar.jsx
@@ -25,6 +25,7 @@ const TrainerCalendar = () => {
         "YYYY-MM-DD h:mm A"
       ).toDate();
       const endDateTime = dayjs(startDateTime).add(50, "minute").toDate();
+
       return {
         title: `${trainer.name} - Yoga Class`,
         start: startDateTime,
@@ -34,6 +35,13 @@ const TrainerCalendar = () => {
       };
     })
   );
+  const testEvents = [
+    {
+      title: "Test Event",
+      start: new Date(2024, 11, 15, 12, 0), // Aralık 15, 2024, 12:00
+      end: new Date(2024, 11, 15, 12, 50), // Aralık 15, 2024, 12:50
+    },
+  ];
 
   const handleEventSelect = (event) => {
     setSelectedEvent(event);
@@ -45,7 +53,7 @@ const TrainerCalendar = () => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center mt-8">
+      <div className="flex flex-col justify-center items-center mt-8 p-2">
         <div className="flex flex-row mb-8 space-x-2">
           {filteredTrainers.map((ftrainer, idx) => (
             <Button key={idx} onClick={() => handleTrainerSelecet(ftrainer)}>
@@ -55,11 +63,13 @@ const TrainerCalendar = () => {
         </div>
         <Calendar
           localizer={localizer}
-          events={events}
+          events={testEvents}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: "500px", width: "90%" }}
+          style={{ height: 500, width: "90%", overflow: "visible" }}
           onSelectEvent={handleEventSelect}
+          defaultDate={new Date()}
+          defaultView="month"
         />
       </div>
       <div>
